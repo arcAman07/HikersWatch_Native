0eb64ffe99b6a820df6e021f95475e27b49aff89
Aman Sharma <76823502+arcAman07@users.noreply.github.com>
2021-09-24 20:11:40 +0530

    Add files via upload

---
+++ product.js
@@ -0,0 +1,17 @@
+const products = []
+
+module.exports = class Product {
+    constructor(title) {
+        this.title = title;
+    }
+
+    save() {
+        products.push(this);
+    }
+
+    static fetchAll() {
+        return products;
+
+    }
+
+}
